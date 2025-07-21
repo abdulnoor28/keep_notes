5229318344a156aab998c63e92a2343b45bd4b94
abdulnoor28 <noorahamadnoor726@gmail.com>
2025-07-21 19:13:33 +0530

    now this project completely ready

--- To_do_list.py
+++ To_do_list.py
@@ -1,6 +1,6 @@
 
 while True:
-    choices=["1.Add a Task","2.View Tasks","3.Delete a Task","4.Exit"]
+    choices=["1.Add a Task.","2.View Tasks.","3.Delete a Task.","4.mark a task as completed.","5.show me the pending task only","6.show me the completed tasks only.","7.exit"]
     for choice in choices:
         print(choice)
     user_choice=int(input("enter your choice (1-4): "))
@@ -44,10 +44,47 @@ while True:
         except FileNotFoundError:
             print("No tasks found. Please add a task first.")
             continue
-    elif(user_choice==4):
+    elif(user_choice==7):
         print("Exiting the program. Goodbye!")
         exit()
-
+    elif(user_choice==4):
+        with open("notes.txt","r") as file:
+            content=file.readlines()
+        print("tasks:")
+        for index, line in enumerate(content, start=1):
+            print(f"{index}. {line.strip()}")
+        print("\n")
+        user=int(input("enter the index which you want to mark as completed:"))
+        if 1<=user<=len(content):
+            old_line=content[user-1].strip()
+            updated_line=old_line+" ( completed )\n"
+            content[user-1]=updated_line
+            
+            with open("notes.txt","w") as file:
+                file.writelines(content)
+                
+            print(f"\nLine {user} marked as updated!")
+        
+        else:
+            print("invalid line number.try again")
+            continue
+    elif(user_choice==5):
+        with open("notes.txt","r") as file:
+            lines=file.readlines()
+        print("pending tasks:")
+        for line in lines:
+            if "completed" not in line:
+                print(line.strip())
+        print("\n")
+    elif(user_choice==6):
+        with open("notes.txt","r") as file:
+            lines=file.readlines()
+        print("completed tasks:")
+        for line in lines:
+            if "completed" in line:
+                print(line.strip())
+        print("\n")
+        
     else:
         print("incorrect choice. Please try again.")
         continue
